ff1d397f4d18c4c024b8a58d9ebe54ac386b6e63
behramialaudin <72756138+behramialaudin@users.noreply.github.com>
2021-01-15 03:20:23 +0100

    created mytask.css file and edited mytask.php
    styled our timeline platform

--- mytask.php
+++ mytask.php
@@ -3,9 +3,6 @@ session_start();
 
 $config['sess_expire_on_close'] = true;
 
-
-
-
 require_once "functions.php";
 
 if (!isUserLoggedIn()) {
@@ -18,17 +15,36 @@ if (!isUserLoggedIn()) {
 <head>
     <title>My Task </title>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="mytask.css">
 </head>
 
 <body>
-<center>
-    <label>Title: </label> <br>
-    <input type="text"> <br>
-    <label>Description: </label> <br>
-    <input type="tex"> <br> <br>
-    <input type="submit" value="submit">
 
-    <a href="#" id="signout">Sign out!</a>
+
+
+        <h1>My Task</h1>
+        <button id="add_new_button">Add New</button>
+
+    <button href="#" id="signout">Sign out!</button>
+<form id="new_menu_item_form" style="display: none;">
+        <label>Title:</label><br>
+        <input id="new_menu_item_form_title" type="text" /><br>
+        <label>Description:</label><br>
+        <textarea id="new_menu_item_form_description"></textarea> <br>
+        <select name="" id="select_dropdown">
+            <option value="to_do"> To Do</option>
+            <option value="in_progress">In Progress</option>
+            <option value="done" selected>Done</option>
+
+
+        </select><br>
+
+        <button id="new_menu_item_close_button" type="button" value="Close">Close</button>
+        <button id="new_menu_item_add_button" type="button" value="Add">Add</button>
+
+    </form>
+
+
 
 </body>
 
@@ -40,6 +56,17 @@ if (!isUserLoggedIn()) {
         })
 
     })
+
+    $("#add_new_button").click(function(){
+    $("#new_menu_item_form").css("display", "block");
+});
+
+$("#new_menu_item_close_button").click(function () {
+    $("#new_menu_item_form").css("display", "none");
+});
+
 </script>
+
+
 
 </html>
