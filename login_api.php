d27b19ad8f8873708b4f255f70f90446cd216654
behramialaudin <72756138+behramialaudin@users.noreply.github.com>
2021-01-17 13:35:09 +0100

    update login_api.php
    saved user_id in session

--- login_api.php
+++ login_api.php
@@ -1,51 +1,54 @@
 <?php
-    session_start();
+session_start();
 
-    require_once "db.php";
-    require_once "functions.php";
-
-
-    header('Content-type: application/json');
-
-    if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-        echo json_encode([
-            'success' => false,
-            'message' => 'POST HTTP METHOD REQUIRED'
-        ]);
-        die(); 
-   }
-
-    // if logged in then redirect to timeline.php
-    if (isUserLoggedIn()) {
-        echo json_encode([
-            'success' => false,
-            'message' => 'User is already athenticated'
-        ]);
-        die(); 
-    }
-
-    // get the data
-    $email = $_POST['email'];
-    $password= $_POST['password'];
-
-    $user = findUserByEmailAndPassword($email, $password);
+require_once "db.php";
+require_once "functions.php";
 
 
-    if($user != null){
-        $_SESSION['logged_in'] = true;
-        $_SESSION['email'] = $email;
+header('Content-type: application/json');
 
-        echo json_encode([
-            'success' => true,
-            'message' => 'Authenticated'
-        ]);
-        die();
-        
-    } else {
-        $_SESSION['logged_in'] = false;
-        echo json_encode([
-            'success' => false,
-            'message' => 'Wrong credentials!!'
-        ]);
-        die();
-    }
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    echo json_encode([
+        'success' => false,
+        'message' => 'POST HTTP METHOD REQUIRED'
+    ]);
+    die();
+}
+
+// if logged in then redirect to timeline.php
+if (isUserLoggedIn()) {
+    echo json_encode([
+        'success' => false,
+        'message' => 'User is already athenticated'
+    ]);
+    die();
+}
+
+// get the data
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$user = findUserByEmailAndPassword($email, $password);
+
+
+if ($user !== null) {
+    // logged in
+    $_SESSION['logged_in'] = true;
+    $_SESSION['email'] = $email;
+    $_SESSION['user_id'] = $user['user_id'];
+
+    echo json_encode([
+        'success' => true,
+        'message' => 'Authenticated'
+    ]);
+    die();
+    
+} else {
+    $_SESSION['logged_in'] = false;
+    echo json_encode([
+        'success' => false,
+        'message' => 'Wrong crendentials!'
+    ]);
+    die();
+}
+?>
